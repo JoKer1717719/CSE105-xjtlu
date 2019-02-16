583f53abb8ff400e2c163a713f0e54b68ab8e182
JoKer1717719 <42902211+JoKer1717719@users.noreply.github.com>
2019-02-16 16:38:03 +0800

    Add files via upload

---
+++ Week 1/Calculations.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Calculations {
+    public static void main(String[] args) {
+        //From Strings to Arithmetic
+        System.out.println("hello");
+        //System.out.println("5 * 7 + 4 - 2 / 2 = 38");
+        int num1 = 5;
+        int num2 = 7;
+        int num3 = 4;
+        int num4 = 2;
+        int result;
+        result = num1 * num2 + num3 - num4 / num4;
+        System.out.println("The result is: " + result );
+        System.out.println(num1 + "*" + num2 + "+" + num3 + "-" + num4 + "/" + num4 + "= " + result );
+}  
+}
